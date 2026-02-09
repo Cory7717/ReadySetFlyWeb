975c192f34f750bcd7cc791fd37c3d12475e1fa7
Cory Armer <coryarmer@gmail.com>
2026-02-09 10:37:43 -0600

    Image fix

--- client/src/pages/create-marketplace-listing.tsx
+++ client/src/pages/create-marketplace-listing.tsx
@@ -673,7 +673,7 @@ export default function CreateMarketplaceListing() {
                 render={({ field }) => (
                   <FormItem>
                     <FormLabel>Category</FormLabel>
-                    <Select onValueChange={field.onChange} defaultValue={field.value}>
+                    <Select onValueChange={field.onChange} value={field.value || ""}>
                       <FormControl>
                         <SelectTrigger data-testid="select-category">
                           <SelectValue placeholder="Select a category" />

cec863bdba59774968e424b5e155e01157f54b1a
Cory Armer <coryarmer@gmail.com>
2026-02-26 14:56:47 -0600

    TFR Fix

--- server/routes.ts
+++ server/routes.ts
@@ -1425,7 +1425,7 @@ const tfmsCache = new Map<string, { expiresAt: number; value: any }>();
 const FAA_TFR_ARCGIS_URLS = [
   ...(TFR_ARCGIS_PROXY_URL ? [TFR_ARCGIS_PROXY_URL] : []),
   ...TFR_ARCGIS_URLS,
-  "https://services1.arcgis.com/n4Ot9Qz0t5espY4s/ArcGIS/rest/services/FAA_TFRs/FeatureServer/0/query",
+  "https://services1.arcgis.com/n4Ot9Qz0t5espY4s/arcgis/rest/services/FAA_TFRs/FeatureServer/0/query",
   "https://gis.faa.gov/arcgis/rest/services/TFMS/TFR/MapServer/0/query",
   "https://tfr.faa.gov/tfr_map_ims/MapServer/0/query",
 ].filter((value, index, arr) => value && arr.indexOf(value) === index);
@@ -1818,7 +1818,7 @@ async function fetchArcGisTfrs(bbox?: { minLon: number; minLat: number; maxLon: 
     spatialRel: "esriSpatialRelIntersects",
     inSR: "4326",
     outSR: "4326",
-    f: "geojson",
+    f: "json",
   });
 
   for (const baseUrl of FAA_TFR_ARCGIS_URLS) {
